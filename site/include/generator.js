9cb3c8f774360414b1f65728c2ad9a836addaa9c
Miyo Sho <135030944+yuri-kiss@users.noreply.github.com>
2024-08-05 21:58:03 -0400

    Actually fix the header being below the warnings

--- site/include/generator.js
+++ site/include/generator.js
@@ -60,6 +60,7 @@ GeneratorLoaded(new (function Generator() {
       h1_div.textContent = document.querySelector('title').textContent;
       h1.appendChild(h1_img);
       h1.appendChild(h1_div);
+      wrap.appendChild(h1);
       /*// Archive warning
       const archive = document.createElement('div');
       archive.classList.add('infobox', 'infobox-warning-orange');
@@ -84,7 +85,6 @@ GeneratorLoaded(new (function Generator() {
       warning.appendChild(warning_body);
       wrap.appendChild(warning);
       // Return the wrap
-      wrap.appendChild(h1);
       return wrap;
     };
     this.removeHeaderImage = () => {
