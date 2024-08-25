f96c5f6cecc89f75fc1889f27be3e750386b4de4
みよ <135030944+yuri-kiss@users.noreply.github.com>
2024-08-24 20:06:06 -0400

    Prettier

--- site-files/extensions/Lemon/GalleryAPI.js
+++ site-files/extensions/Lemon/GalleryAPI.js
@@ -1,72 +1,72 @@
-(function(Scratch) {
-  `use strict`;
+(function (Scratch) {
+  'use strict';
 
-   if(!Scratch.extensions.unsandboxed) {
-     throw new Error("The 'History Api Extension' requires to be unsandboxed!")
-   }
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error("The 'History Api Extension' requires to be unsandboxed!");
+  }
 
   class HistoryAPI {
     getInfo() {
       return {
         id: 'lemonHistory',
         name: 'Web History API',
-        docsURI : 'https://developer.mozilla.org/en-US/docs/Web/API/History_API',
+        docsURI: 'https://developer.mozilla.org/en-US/docs/Web/API/History_API',
         blocks: [
           {
             opcode: 'history',
             blockType: Scratch.BlockType.REPORTER,
-            text: 'window history'
+            text: 'window history',
           },
           {
             opcode: 'back',
             blockType: Scratch.BlockType.COMMAND,
-            text: 'go back'
+            text: 'go back',
           },
           {
             opcode: 'forward',
             blockType: Scratch.BlockType.COMMAND,
-            text: 'go forward'
+            text: 'go forward',
           },
           {
             opcode: 'go',
             blockType: Scratch.BlockType.COMMAND,
             text: 'go [times]',
             arguments: {
-               times: {
-                  type: Scratch.ArgumentType.NUMBER
-               }
-            }
+              times: {
+                type: Scratch.ArgumentType.NUMBER,
+              },
+            },
           },
           {
             opcode: 'state',
             blockType: Scratch.BlockType.REPORTER,
-            text: 'history state'
+            text: 'history state',
           },
           {
             opcode: 'push',
             blockType: Scratch.BlockType.COMMAND,
             text: 'push [pageID] [userID] with link [url]',
             arguments: {
-               pageID: {
-                  type: Scratch.ArgumentType.NUMBER,
-                  defaultValue: 1
-               },
-                userID: {
-                 type: Scratch.ArgumentType.NUMBER,
-                 defaultValue: 5
-               },
-               url: {
+              pageID: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 1,
+              },
+              userID: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 5,
+              },
+              url: {
                 type: Scratch.ArgumentType.STRING,
-                defaultValue: "https://turbowarp.org/"
-               }
-            }
+                defaultValue: 'https://turbowarp.org/',
+              },
+            },
           },
           {
             opcode: 'supported',
             blockType: Scratch.BlockType.BOOLEAN,
-            text: 'api supported?'
+            text: 'api supported?',
           },
-        ]
+        ],
       };
     }
 
@@ -88,7 +88,7 @@
     push(args) {
       const state = { page_id: Scratch.Cast.toNumber(args.pageID), user_id: Scratch.Cast.toNumber(args.userID) };
       const url = Scratch.Cast.toString(args.url);
-      window.history.pushState(state,"",url);
+      window.history.pushState(state, '', url);
     }
     state() {
       return JSON.stringify(window.history.state);
