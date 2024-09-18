3777c10a2cbfc15f0b37a98be272461e74ea1667
みよ <135030944+yuri-kiss@users.noreply.github.com>
2024-09-18 05:35:15 -0400

    Disable connections

---
+++ site-files/extensions/0znzw/tests/!hidden/disableConnections.js
@@ -0,0 +1,61 @@
+/**!
+ * Disable connections test
+ * @author 0znzw https://scratch.mit.edu/users/0znzw/
+ * @version 1.0
+ * @copyright MIT & LGPLv3 License
+ * Do not remove this comment
+ */
+(function(Scratch) {
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error(`"Disable connections" must be ran unsandboxed.`);
+  }
+  const extId = '0znzwDisableConnectionsTest';
+  const { BlockType, ArgumentType } = Scratch;
+  const bypassCheck = ['text'];
+  if (Scratch.gui) Scratch.gui.getBlockly().then(ScratchBlocks => {
+    const { Extensions, Connection } = ScratchBlocks;
+    Extensions.register(`${extId}_disableInputConnections`, function() {
+      this.disableInputConnections = true;
+    });
+    Connection.REASON_DISABLED_INPUTS = -259;
+    const ccwr_ = Connection.prototype.canConnectWithReason_;
+    Connection.prototype.canConnectWithReason_ = function(...args) {
+      const [attempt] = args;
+      if (attempt.sourceBlock_.disableInputConnections && !bypassCheck.includes(this.sourceBlock_.type)) return Connection.REASON_DISABLED_INPUTS;
+      return ccwr_.apply(this, args);
+    };
+    const cc_ = Connection.prototype.checkConnection_;
+    Connection.prototype.checkConnection_ = function(...args) {
+      const [attempt] = args;
+      const ccwr = this.canConnectWithReason_(attempt);
+      if (ccwr === Connection.REASON_DISABLED_INPUTS) throw 'Attempted to connect to a block with disabled inputs.';
+      return cc_.apply(this, args);
+    };
+  });
+  class extension {
+    getInfo() {
+      return {
+        id: extId,
+        name: 'Disable connections',
+        blocks: [{
+          opcode: 'nope',
+          blockType: BlockType.COMMAND,
+          text: 'nah [abc] cant [123] blocks',
+          arguments: {
+            abc: {
+              type: ArgumentType.STRING,
+              defaultValue: 'you',
+            },
+            123: {
+              type: ArgumentType.STRING,
+              defaultValue: 'connect',
+            },
+          },
+          extensions: [`${extId}_disableInputConnections`],
+        }],
+      };
+    }
+    nope() {}
+  }
+  Scratch.extensions.register(new extension());
+})(Scratch);
