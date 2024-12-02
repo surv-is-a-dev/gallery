bb95303ffe842876dfd37d5758ae4fde8a2073e7
Ashime Sho <a_2oo+gituseremail@outlook.com>
2024-12-02 15:19:59 -0500

    Onee-sann looky

---
+++ site-files/extensions/0znzw/tests/!idk/SandboxSkipper.js
@@ -0,0 +1,43 @@
+/**!
+ * Sandbox Skipper
+ * @author 0znzw https://scratch.mit.edu/users/0znzw/
+ * @version 1.0
+ * @copyright MIT License
+ * Do not remove this comment
+ */
+(function (Scratch) {
+  'use strict';
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error(`"Sandbox Skipper" extension must be ran unsandboxed.`);
+  }
+  class extension {
+    constructor() {
+      this.doPatch(Scratch);
+      Scratch.vm.on('SETUP_UNSANDBOXED_EXTENSION_API', (Scratch) => {
+        this.doPatch(Scratch);
+      });
+    }
+    getInfo() {
+      return {
+        id: '0znzwSandboxSkipperTest',
+        name: 'Sandbox Skipper',
+        blocks: [{
+          blockType: Scratch.BlockType.EVENT,
+          opcode: 'saver',
+          text: 'put me in the project to save.',
+          isTerminal: true,
+          isEdgeActivated: false,
+        }],
+      };
+    }
+    doPatch(Scratch) {
+      Scratch.extensions.unsandboxed = false;
+      const vm = Scratch.vm;
+      vm.securityManager.__proto__.getSandboxMode = async () => 'unsandboxed';
+      vm.securityManager.getSandboxMode = async () => 'unsandboxed';
+      vm.extensionManager.securityManager = vm.securityManager;
+    }
+    saver() { return false; }
+  }
+  Scratch.extensions.register(new extension());
+})(Scratch);
