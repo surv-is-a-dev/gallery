5a277aefa6c50ec7b83a0646936d5bf17149b754
yuri-kiss <135030944+yuri-kiss@users.noreply.github.com>
2025-04-21 17:40:24 -0400

    format and fix right click menu

--- site-files/extensions/0znzw/RightClickMenu.js
+++ site-files/extensions/0znzw/RightClickMenu.js
@@ -1,41 +1,59 @@
 /**!
  * Right Click Menu
  * @author 0znzw <meow@miyo.lol> (@link https://scratch.mit.edu/users/0znzw/)
- * @version 1.0
+ * @version 1.1
  * @license MIT AND LGPL-3.0
  * Do not remove this comment
  */
 (function(Scratch) {
   'use strict';
   if (!Scratch.extensions.unsandboxed) {
-    throw new Error(`"RightClick-Menu" extension must be ran unsandboxed.`);
+    throw new Error(`"Right click menu" extension must be ran unsandboxed.`);
   }
-  const disable = function(e){e.preventDefault(); return false}, enable = function(e){return true};
-  const fakeEvent = {preventDefault: function(){}};
-  document.oncontextmenu = enable;
-  let isDisabled = false;
+  let $isDisabled = false;
+  const document_oncontextmenu = document.oncontextmenu;
+  document.oncontextmenu = function oncontextmenu(event) {
+    if ($isDisabled) event.preventDefault();
+    if (document_oncontentmenu) document_oncontentmenu(event);
+    return !$isDisabled;
+  };
+  const { BlockType } = Scratch;
+  const extId = '0znzwRightClick';
   class extension {
-    getInfo() { return({
-      id: '0znzwRightClick',
-      name: 'Right Click menu',
-      blocks: [{
-        blockType: Scratch.BlockType.COMMAND,
-        opcode: 'enable',
-        text: 'enable right click menu'
-      }, {
-        blockType: Scratch.BlockType.COMMAND,
-        opcode: 'disable',
-        text: 'disable right click menu'
-      }, {
-        blockType: Scratch.BlockType.BOOLEAN,
-        opcode: 'isDisabled',
-        text: 'is right click menu disabled?'
-      }]
-    })}
-    // @ts-ignore
-    isDisabled() { return(!document.oncontextmenu(fakeEvent)) }
-    enable() { document.oncontextmenu = enable; }
-    disable() { document.oncontextmenu = disable; }
+    getInfo() {
+      return {
+        id: extId,
+        name: 'Right click menu',
+        blocks: [{
+          blockType: BlockType.COMMAND,
+          opcode: 'enable',
+          text: 'enable right click menu',
+        }, {
+          blockType: BlockType.COMMAND,
+          opcode: 'disable',
+          text: 'disable right click menu',
+        }, {
+          blockType: BlockType.BOOLEAN,
+          opcode: 'isDisabled',
+          text: 'is right click menu disabled?',
+        }],
+      };
+    }
+    isDisabled() {
+      try {
+        return !document.oncontextmenu(new MouseEvent(0));
+      } catch(e) {
+        console.warn('Failed to get context menu disable status.', e);
+        return $isDisabled;
+      }
+    }
+    enable() { $isDisabled = false; }
+    disable() { $isDisabled = true; }
   }
-  Scratch.extensions.register(new extension());
+
+  const ext = new extension();
+  if (Scratch.extensions.unsandboxed) {
+    Scratch.vm.runtime[`cext_${extId}`] = ext;
+  }
+  Scratch.extensions.register(ext);
 })(Scratch);
